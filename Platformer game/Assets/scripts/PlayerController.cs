a1c6663d11793def703441e11bf619af745abd2a
TheIntriguedProgrammer <145798097+TheIntriguedProgrammer@users.noreply.github.com>
2023-11-14 13:50:26 -0500

    Changes made on 11/14/23

--- Platformer game/Assets/scripts/PlayerController.cs
+++ Platformer game/Assets/scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Animation variables 
+    Animator anim;
+    public bool moving = false;
+
+
+
+
+    // Movement Variables
     Rigidbody2D rb; // create reference for the rigidbody becuase jump requires physics
     public float jumpForce;// the force that will be added to the vertical component of player's velocity
     public float speed;
@@ -22,6 +30,9 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+
     }
 
     // Update is called once per frame
@@ -40,19 +51,27 @@ public class PlayerController : MonoBehaviour
             // this is done by change to sign of the scale between -ive and +ive
             newScale.x = -currentScale;
 
-        
-                
-        
+            // this line of code is to activate the walk animation
+            moving = true;
         }
+        
         if (Input.GetKey("d")|| Input.GetKey(KeyCode.RightArrow)) 
         {
             newPosition.x += speed;
 
             newScale.x = currentScale;
+
+            //controlls walking animation
+            moving = true;
         
         
         
         }
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d") || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            moving = false;
+        }
+
 
         if ((Input.GetKeyDown("w")|| Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded == true)
             // get keydown check for the inital key press
@@ -63,6 +82,10 @@ public class PlayerController : MonoBehaviour
 
         }
 
+
+
+
+        anim.SetBool("isMoving", moving);
         transform.position = newPosition;
         transform.localScale = newScale;
 
